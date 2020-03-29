9f290cdf2646dd7576d10810d8b4a259846cecaa
David Glass <david.michael.glass@gmail.com>
2020-03-29 19:10:39 +0000

    Implemented TCLink; bandwidth accounted for (kinda)

--- basic.py
+++ basic.py
@@ -5,6 +5,7 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+from mininet.link import TCLink
 
 class Basic_Topo(Topo):
     #Two hosts with two switches in between and
@@ -19,13 +20,13 @@ class Basic_Topo(Topo):
         s2 = self.addSwitch('s2')
 
         info('*** Adding links\n')
-        self.addLink(h1, s1)
-        self.addLink(s1, s2)
-        self.addLink(s2, h2)
+        self.addLink(h1, s1, bw=.2)#, max_queue_size=20)
+        self.addLink(s1, s2, bw=.05)
+        self.addLink(s2, h2, bw=.2)
 
 def run_network():
     topo = Basic_Topo()
-    net = Mininet(topo)
+    net = Mininet(topo, link=TCLink)
     net.start()
     info('*** Starting network\n')
     print("Dumping host connections")
