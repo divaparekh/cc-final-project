45fd39b2a45929fb4c99cc526b82098db0730349
divaparekh <dvparekh11@mininet.us-central1-a.c.final-project-271900.internal>
2020-04-23 15:13:59 +0000

    starting shell script

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 git clone git://github.com/mininet/mininet
 mininet/util/install.sh -a
-sudo apt install python3-pip
-sudo apt install python-numpy
-sudo apt install python-pandas
-sudo apt install python-matplotlib
+cp ./cc-final-project/basic.py ./mininet/custom/
+sudo apt -y install python3-pip
+sudo apt -y install python-numpy
+sudo apt -y install python-pandas
+sudo apt -y install python-matplotlib
 cd cc-final-project
 pip3 install jupyter
-~/.local/bin/jupyter-notebook password
-read password
-read password
+
+# Mininet run lines go here
+cd ..
+sudo python mininet/custom/basic.py 50 5
+pingall
+
+cd cc-final-project
+~/.local/bin/jupyter-notebook run-group9.ipynb --ip=0.0.0.0
