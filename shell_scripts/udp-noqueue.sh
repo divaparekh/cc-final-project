858f401d041784ef0b3fa988cd03a262af60ee9f
Diva Parekh <divaparekh@mininet.us-central1-a.c.final-project-271900.internal>
2020-04-25 20:47:50 +0000

    organizing

--- shell_scripts/udp-noqueue.sh
+++ shell_scripts/udp-noqueue.sh
@@ -1,2 +1,2 @@
-h2 iperf -s --udp --len 300 -fk -e -p 5001 -i 5 --time 30 > /home/divaparekh/cc-final-project/server_outputs/noqueue &
+h2 iperf -s --udp --len 300 -fk -e -p 5001 -i 5 --time 30 > /home/divaparekh/cc-final-project/server_outputs/udp-noqueue &
 h1 iperf -c h2 --udp --len 300 -fk --interval 5 --time 30 --listenport 5001 -P 6
