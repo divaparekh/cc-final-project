01fb127a1a18d1708e8f19ab6cc79fc25ddceb77
Diva Parekh <divaparekh@mininet.us-central1-a.c.final-project-271900.internal>
2020-04-28 07:27:42 +0000

    FULL DATASET

--- shell_scripts/mininet.sh
+++ shell_scripts/mininet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo mn -c
-sudo python mininet/custom/basic.py 2 1.0
+sudo python mininet/custom/diva-basic.py 2 1
 sudo mn -c
-sudo python mininet/custom/basic.py 2 2.5
+sudo python mininet/custom/diva-basic.py 2 2.5
 sudo mn -c
-sudo python mininet/custom/basic.py 2 5.0
+sudo python mininet/custom/diva-basic.py 2 5
 sudo mn -c
-sudo python mininet/custom/basic.py 2 10.0
+sudo python mininet/custom/diva-basic.py 2 10
 sudo mn -c
 sudo python convert_ditg_logs.py
