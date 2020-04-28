accf4f17dea41e009f0e5b87b530177a31115430
Diva Parekh <divaparekh@mininet.us-central1-a.c.final-project-271900.internal>
2020-04-28 04:44:18 +0000

    HI

--- shell_scripts/ditg-tcp-codel-5.sh
+++
@@ -1,1 +0,0 @@
-py net.get('h2').cmd('./D-ITG-2.8.1-r1023/bin/ITGRecv &')
--- shell_scripts/tcp-codel.sh
+++
@@ -1,2 +0,0 @@
-h2 iperf -s --len 300 -fk -e -p 5001 -i 5 --time 30 > /home/divaparekh/cc-final-project/server_outputs/tcp-codel &
-h1 iperf -c h2 --len 300 -fk --interval 5 --time 30 --listenport 5001 -P 6
