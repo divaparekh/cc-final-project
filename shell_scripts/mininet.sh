0aa58a77640c4b6f4bbdfa4b93019dc35825d783
Diva Parekh <divaparekh@mininet.us-central1-a.c.final-project-271900.internal>
2020-04-29 05:52:56 +0000

    fq codel

--- shell_scripts/mininet.sh
+++ shell_scripts/mininet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo mn -c
-sudo python mininet/custom/basic.py 2 1
+sudo python mininet/custom/basic.py $1 1
 sudo mn -c
-sudo python mininet/custom/basic.py 2 2.5
+sudo python mininet/custom/basic.py $1 2.5
 sudo mn -c
-sudo python mininet/custom/basic.py 2 5
+sudo python mininet/custom/basic.py $1 5
 sudo mn -c
-sudo python mininet/custom/basic.py 2 10
+sudo python mininet/custom/basic.py $1 10
 sudo mn -c
 sudo python cc-final-project/convert_ditg_logs.py
 #sudo rm ~/cc-final-project/server_outputs/*.log
