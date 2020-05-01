8f48a01ad20a45a928cac75656af3ce109ae478c
Diva Parekh <dvparekh11@david-diva-antara.us-central1-a.c.final-project-271900.internal>
2020-05-01 00:56:59 +0000

    FUCK

--- shell_scripts/all.sh
+++ shell_scripts/all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ~/.
+sudo apt-get install build-essential
 git clone git://github.com/mininet/mininet
 mininet/util/install.sh -a
 cp ./cc-final-project/basic.py ./mininet/custom/
@@ -8,10 +9,12 @@ pip3 install pandas
 pip3 install matplotlib
 pip3 install jupyter
 wget http://www.grid.unina.it/software/ITG/codice/D-ITG-2.8.1-r1023-src.zip
+sudo apt-get install unzip
 unzip D-ITG-2.8.1-r1023-src.zip
 cd D-ITG-2.8.1-r1023/src
 make
 cd ~/.
+sudo rm D-ITG-2.8.1-r1023-src.zip
 sudo mn -c
 sudo python mininet/custom/basic.py $1 1
 sudo mn -c
